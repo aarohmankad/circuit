9322b5c4dffff689bc98d7135710a4c85a99a7f3
TigheO <uberouroboros@gmail.com>
2015-03-05 10:25:12 -0800

    Lay down Level one

--- game/Assets/Scripts/playerControls.js
+++ game/Assets/Scripts/playerControls.js
@@ -1,9 +1,7 @@
 
 var velocity : Vector3 = Vector3.zero;
-var gravity : float = 20.0;
+var gravity : float = 5.0;
 var moveRight = true;
-var running = false;
-var movingTime : float;
 var jumpSound : AudioClip;
 var dieSound : AudioClip;
 var soundRate : float = 0.0;
@@ -27,88 +25,59 @@ function Update()
 	if (controller.isGrounded)
 	{
 		
-		velocity = Vector3(Input.GetAxis("Horizontal"),0,0);
+		velocity = Vector3(Input.GetAxis("Horizontal") * 2,0,0);
 		if (velocity.x > 0)
 		{
 			moveRight = true;
-			movingTime += Time.deltaTime;
-			if (movingTime > 1.0)
-				running = true;
-			else
-				running = false;
 		}
 		else if (velocity.x < 0)
 		{
-			movingTime += Time.deltaTime;
 			moveRight = false;
-			if (movingTime > 1.0)
-				running = true;
-			else
-				running = false;			
-		}
-		else
-		{
-			movingTime = 0;
 		}
 		
 		if (Input.GetKey("space"))
 		{
 			PlaySound(jumpSound,0);
-			velocity.y = 8;
+			velocity.y = 12;
 			if (moveRight)
 			{
-				aniPlay.aniSprite(16,16,11,2,4,12,false);   //jump face right
+				aniPlay.aniSprite(3,9,0,2,3,8,false);   //jump face right
 			}
 			else
 			{
-				aniPlay.aniSprite(16,16,11,3,4,12,false);   //jump face left
+				aniPlay.aniSprite(3,9,0,2,3,8,true);   //jump face left
 			}
 		}		
 		else
 		{
 			if (Input.GetKey("right"))
 			{
-				if (running)
-				{
-					aniPlay.aniSprite(16,16,0,6,16,12,false);   //run face right		
-				}
-				else
-				{
-					aniPlay.aniSprite(16,16,0,2,10,12,false);   //walk face right		
-				}
+				aniPlay.aniSprite(3,9,0,1,3,8,false);   //walk face right
 			}
 			else if (Input.GetKey("left"))
 			{
-				if (running)
-				{
-					aniPlay.aniSprite(16,16,0,7,16,12,false);   //run face left		
-				}
-				else
-				{
-					aniPlay.aniSprite(16,16,0,3,10,12,false);   //walk face left		
-				}	
-					
+				aniPlay.aniSprite(3,9,0,1,3,8,true);   //walk face left
 			}
 			else if (Input.GetKey("down"))
 			{
 				if (moveRight)
 				{
-					aniPlay.aniSprite(16,16,0,8,16,12,false);   //crouch face right		
+					aniPlay.aniSprite(3,9,0,6,3,24,false);   //crouch face right		
 				}	
 				else
 				{
-					aniPlay.aniSprite(16,16,0,9,16,12,false);   //crouch face left	
+					aniPlay.aniSprite(3,9,0,6,3,24,false);   //crouch face left	
 				}
 			}		
 			else
 			{
 				if (moveRight)
 				{
-					aniPlay.aniSprite(16,16,0,0,16,12,false);   //neutral face right		
+					aniPlay.aniSprite(3,9,0,0,3,8,false);   //neutral face right		
 				}
 				else
 				{
-					aniPlay.aniSprite(16,16,0,1,16,12,false);   //neutral face left	
+					aniPlay.aniSprite(3,9,0,0,3,8,true);   //neutral face left	
 				}	
 			}
 		 }
